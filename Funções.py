ede4df1ea3e765d241079d76dc5bd899a96d5c76
AdrianJQuindeler <adrianquindeler@gmail.com>
2024-03-28 12:00:41 -0300

    Criando base para o carrinho

--- Funções.py
+++ Funções.py
@@ -2,40 +2,49 @@ from PyQt5.QtWidgets import QLabel, QPushButton
 from PyQt5 import QtGui
 
 class Funcoes:
-    def createImage(self, leftDistance, topDistance, path, width = 100, height = 100):
-        image = QLabel(self)
+    def createImage(self, parent, leftDistance, topDistance, path, width = 100, height = 100):
+        image = QLabel(parent)
         image.move(leftDistance, topDistance)
         image.resize(width, height)
         image.setPixmap(QtGui.QPixmap(path))
         return image
 
-    def createLabel(self, text, leftDistance, topDistance, width, height, style):
-        label = QLabel(self)
+    def createLabel(self, parent, text, leftDistance, topDistance, width, height, style):
+        label = QLabel(parent)
         label.setWordWrap(True)
         label.setText(text)
         label.move(leftDistance, topDistance)
         label.resize(width, height)
         label.setStyleSheet(style)
 
-    def createButton(self, text, leftDistance, topDistance, width, height, style):
-        button = QPushButton(text, self)
+    def createButton(self, parent, text, leftDistance, topDistance, width, height, style):
+        button = QPushButton(text, parent)
         button.move(leftDistance, topDistance)
         button.resize(width, height)
         button.setStyleSheet(style)
         return button
 
-    def createItemToAdd(self, leftDistance, topDistance, path, description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."):
-        image = self.createImage(leftDistance, topDistance, path)
-        label = self.createLabel(description, leftDistance+110, topDistance, 190, 100,
+    def createItemToAdd(self, parent, leftDistance, topDistance, path, description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."):
+        image = self.createImage(parent, leftDistance, topDistance, path)
+        label = self.createLabel(parent, description, leftDistance+110, topDistance, 190, 100,
                                 "QLabel {font: bold; font-size: 14px; color: black; padding: 0 0 0 0}")
-        addToCart = self.createButton("+", leftDistance+90, topDistance+90, 25, 25, 
+        addToCart = self.createButton(parent, "+", leftDistance+90, topDistance+90, 25, 25, 
                             'QPushButton {background-color:#000540; color: white; font:bold; font-size:24px; border-radius:12px; padding: 0 0 5px 0;}')
         return addToCart
 
-    def createItemToAddRemove(self, leftDistance, topDistance, path, description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."):
-        image = self.createImage(leftDistance, topDistance, path)
-        label = self.createLabel(description, leftDistance+110, topDistance, 190, 100,
+    def createItemToAddRemove(self, parent, leftDistance, topDistance, path, description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."):
+        image = self.createImage(parent, leftDistance, topDistance, path)
+        label = self.createLabel(parent, description, leftDistance+110, topDistance, 190, 100,
                                 "QLabel {font: bold; font-size: 14px; color: black; padding: 0 0 0 0}")
-        addToCart = self.createButton("+", leftDistance+90, topDistance+90, 25, 25, 
+        addToCart = self.createButton(parent, "+", leftDistance+90, topDistance+90, 25, 25, 
                             'QPushButton {background-color:#000540; color: white; font:bold; font-size:24px; border-radius:12px; padding: 0 0 5px 0;}')
         return addToCart
+    
+    def createItemInCart(self, parent, leftDistance, topDistance, path, quantity, description = "R$ 00,00"):
+        fundo = self.createLabel(parent, description, leftDistance+110, topDistance, 190, 100,
+                                "QLabel {font: bold; font-size: 14px; color: black; padding: 0 0 0 0}")
+        image = self.createImage(parent, leftDistance, topDistance, path)
+        label = self.createLabel(parent, description, leftDistance+110, topDistance, 190, 100,
+                                "QLabel {font: bold; font-size: 14px; color: black; padding: 0 0 0 0}")
+        quantity = self.createLabel(parent, quantity, leftDistance+90, topDistance+90, 25, 25, 
+                            ("QLabel {font: bold; background: black; font-size: 45px; color: white;  padding: 10px}"))
--- TelaInicial.py
+++ TelaInicial.py
@@ -5,51 +5,48 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.ProductList = []
-        self.Quantity = {"pc":0, "teclado":0, "mouse":0, "monitor":0, "mousepad":0, "processador":0,
-                        "ssd":0, "cooler" :0, "item1":0,   "item2":0}
-        self.windowHeight = 800
-        self.windowWidth = 1280 
+        self.funcoes = Funcoes()
+        self.quantity = {}
+        self.price = {"pc":200, "teclado":10, "mouse":5, "monitor":10, "mousepad":2, "processador":20, "ssd":20, "cooler" :15, "item1":20,   "item2":20}
         self.title = "Adrian's & Adrian's LTDA."
         self.createAllItems()
         self.createButtons()
         self.loadWindow()
 
     def loadWindow(self):
-        self.setGeometry(0, 0, self.windowWidth, self.windowHeight)
+        self.setGeometry(0, 0, 1280,720)
         self.setWindowTitle(self.title)
         self.setStyleSheet("QMainWindow {background: qlineargradient(x1:1, y1:1, x2:0, y2:0, stop: 0 #000540, stop: 1 #1bdeb4); }")
         self.show()
 
-
     def createButtons(self):
 
-        home = Funcoes.createButton(self, "Eletronic Paradise", 950, 10, 320, 50,  
+        home = self.funcoes.createButton(self, "Eletronic Paradise", 950, 10, 320, 50,  
                                 'QPushButton {background-color:#000540; border-radius: 25px; font:bold; font-size:30px; color: white}')
 
-        viewProductDetails = Funcoes.createButton(self, "Ver as especificações", 420, 20, 170, 30, 
+        viewProductDetails = self.funcoes.createButton(self, "Ver as especificações", 420, 20, 170, 30, 
                                 'QPushButton {background-color:#000540; border-radius: 40px; font:bold; font-size:14px; color: white}')
 
-        removeFromCart = Funcoes.createButton(self, "Remover do carrinho", 740, 20, 160, 30,
+        removeFromCart = self.funcoes.createButton(self, "Remover do carrinho", 740, 20, 160, 30,
                                 'QPushButton {background-color:#000540; border-radius: 40px; font:bold; font-size:14; color: white}')
 
-        welcomeLabel = Funcoes.createButton(self, "Seja bem vindo!", 10, 10, 400, 60,
+        welcomeLabel = self.funcoes.createLabel(self, "Seja bem vindo!", 10, 10, 400, 60,
                                 "QLabel {font: bold; font-size: 45px; color: black;  padding: 10px}")
         
         viewProductDetails.clicked.connect(self.on_view_product_details_clicked)
         removeFromCart.clicked.connect(self.on_remove_from_cart_clicked)
     
     def createAllItems(self):  
-        pc = Funcoes.createItemToAdd        (self, 10, 100, "imagens/Medias/pc_m.jpg",          "Supremacy Ultra: o ápice da tecnologia encapsulada em um computador excepcionalmente poderoso e esteticamente impressionante.")
-        teclado = Funcoes.createItemToAdd   (self, 330, 100, "imagens/Medias/teclado_m.jpg",     "Teclado mecânico com uma experiência de digitação excepcional! Combina durabilidade, precisão e conforto em um design elegante e excêntrico.")
-        monitor = Funcoes.createItemToAdd   (self, 650, 100, "imagens/Medias/monitor_m.jpg",     "VisionXtreme: Com resolução Ultra HD 4K, taxa de atualização de 144Hz e tecnologia HDR, oferece imagens nítidas e vibrantes para uma experiência visual imersiva.")
-        mouse = Funcoes.createItemToAdd     (self, 970, 100, "imagens/Medias/mouse_m.jpg",       "PhantomGrip: o Mouse Gamer de Elite Projetado para proporcionar precisão, velocidade e conforto como nenhum outro.")
-        processador= Funcoes.createItemToAdd(self,  10, 240, "imagens/Medias/processador_m.jpg", "HyperCore: Perfeito para Desempenho Computacional! Equipado com tecnologia de ponta, tem potência para lidar com as tarefas mais exigentes")
-        ssd = Funcoes.createItemToAdd       (self, 330, 240, "imagens/Medias/ssd_m.jpg",         "TurboDrive: o SSD de Elite completo! Com capacidades de armazenamento e velocidades de leitura/gravação ultra-rápidas e alta confiabilidade")
-        cooler = Funcoes.createItemToAdd    (self, 650, 240, "imagens/Medias/cooler_m.jpg",      "FrostBlast: o Cooler perfeito para seu Desempenho de Refrigeração! Oferece resfriamento excepcional e silencio para seu sistema.")
-        item1 = Funcoes.createItemToAdd     (self, 970, 240, "imagens/Medias/pc_m.jpg") 
-        mousepad = Funcoes.createItemToAdd  (self,  10, 380, "imagens/Medias/mousepad_m.jpg",    "HyperGlide Pro: o Mousepad de Elite para sua Precisão e Conforto de Movimento!") 
-        item2 = Funcoes.createItemToAdd     (self, 330, 380, "imagens/Medias/pc_m.jpg") 
+        pc = self.funcoes.createItemToAdd        (self, 10, 100, "imagens/Medias/pc_m.jpg",          "Supremacy Ultra: o ápice da tecnologia encapsulada em um computador excepcionalmente poderoso e esteticamente impressionante.")
+        teclado = self.funcoes.createItemToAdd   (self, 330, 100, "imagens/Medias/teclado_m.jpg",     "Teclado mecânico com uma experiência de digitação excepcional! Combina durabilidade, precisão e conforto em um design elegante e excêntrico.")
+        monitor = self.funcoes.createItemToAdd   (self, 650, 100, "imagens/Medias/monitor_m.jpg",     "VisionXtreme: Com resolução Ultra HD 4K, taxa de atualização de 144Hz e tecnologia HDR, oferece imagens nítidas e vibrantes para uma experiência visual imersiva.")
+        mouse = self.funcoes.createItemToAdd     (self, 970, 100, "imagens/Medias/mouse_m.jpg",       "PhantomGrip: o Mouse Gamer de Elite Projetado para proporcionar precisão, velocidade e conforto como nenhum outro.")
+        processador= self.funcoes.createItemToAdd(self,  10, 240, "imagens/Medias/processador_m.jpg", "HyperCore: Perfeito para Desempenho Computacional! Equipado com tecnologia de ponta, tem potência para lidar com as tarefas mais exigentes")
+        ssd = self.funcoes.createItemToAdd       (self, 330, 240, "imagens/Medias/ssd_m.jpg",         "TurboDrive: o SSD de Elite completo! Com capacidades de armazenamento e velocidades de leitura/gravação ultra-rápidas e alta confiabilidade")
+        cooler = self.funcoes.createItemToAdd    (self, 650, 240, "imagens/Medias/cooler_m.jpg",      "FrostBlast: o Cooler perfeito para seu Desempenho de Refrigeração! Oferece resfriamento excepcional e silencio para seu sistema.")
+        item1 = self.funcoes.createItemToAdd     (self, 970, 240, "imagens/Medias/pc_m.jpg") 
+        mousepad = self.funcoes.createItemToAdd  (self,  10, 380, "imagens/Medias/mousepad_m.jpg",    "HyperGlide Pro: o Mousepad de Elite para sua Precisão e Conforto de Movimento!") 
+        item2 = self.funcoes.createItemToAdd     (self, 330, 380, "imagens/Medias/pc_m.jpg") 
 
         pc.clicked.connect         (lambda: self.on_add_to_cart_clicked('pc'))
         teclado.clicked.connect    (lambda: self.on_add_to_cart_clicked('teclado'))
@@ -69,22 +66,27 @@ class MainWindow(QMainWindow):
         print("Botão 'Ver as especificações' clicado.") 
 
     def on_add_to_cart_clicked(self, produto):
-        if len(self.ProductList) == 0:
+        if len(self.quantity) == 0:
             background = QLabel(self)
-            background.move(10, 500)
+            background.move(10, 505)
             background.resize(1260, 200)
             background.setStyleSheet("QLabel {font: bold; font-size: 45px; color: black; background: white; border-radius: 25px; padding: 10px}")
-            finalizePurchase = self.createButton("Finalizar Compra", 1055, 505, 210, 50, 
-                                'QPushButton {background-color:#000540; border-radius: 25px; font:bold; font-size:22px; color: white}')
-            finalizePurchase.clicked.connect(self.on_finalize_purchase_clicked)
+            finalizePurchase = self.funcoes.createButton(self, "Finalizar Compra", 1055, 510, 210, 50, 'QPushButton {background-color:#000540; border-radius: 25px; font:bold; font-size:22px; color: white}')
             background.show()
             finalizePurchase.show()
+        
+        for key, value in self.quantity.items:
+            if(produto == key):
+                value += 1
+        self.quantity[produto] = 1
+        print(self.quantity)
+        #self.funcoes.createItemInCart(self, self.leftDistance, self.topDistance, "imagens/Medias/"+produto+"_p.jpg", self.quantity[produto], "R$ "+self.price[produto])
 
-        self.ProductList.append(produto)
+        finalizePurchase.clicked.connect(lambda: self.on_finalize_purchase_clicked(produto))
         print(f"Produto {produto} adicionado ao carrinho.")
 
     def on_remove_from_cart_clicked(self):
         print("Botão 'Remover produto do carrinho' clicado.")
 
-    def on_finalize_purchase_clicked(self):
-        print("Botão 'Finalizar Compra' clicado.")
+    def on_finalize_purchase_clicked(self, produto):
+        print("A")
